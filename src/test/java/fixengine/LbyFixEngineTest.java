ff21927db7d5bdd8bf3f338e1710c23c86e6719b
edwinlu <edwinbylu@163.com>
2018-07-29 11:05:11 +0800

    created LbyFixEngine class

---
+++ src/test/java/fixengine/LbyFixEngineTest.java
@@ -0,0 +1,16 @@
+package fixengine;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LbyFixEngineTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(LbyFixEngineTest.class);
+
+    @Test
+    public void testMain(){
+        logger.info("testMain is running.");
+    }
+}
